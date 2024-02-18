3c54233add0992885f5eb18f9a4093a6d53177b5
hosuk-dev <61938944+hosuk-dev@users.noreply.github.com>
2024-02-18 16:15:21 +0900

    Update GitTestApplication.java

--- src/main/java/com/example/git_test/GitTestApplication.java
+++ src/main/java/com/example/git_test/GitTestApplication.java
@@ -11,8 +11,8 @@ public class GitTestApplication {
     }
 
     public void test() {
-        String userId = "ddd";
-        String userPwd = "ddd";
+        String userId = "remote";
+        String userPwd = "remote";
     }
 
 }
